0125ccdafdc26847352ecfc485883794aa1abf87
Samuel Custódio <samuelcustodioes@gmail.com>
2025-10-12 17:57:38 -0300

    feat: adicionar suporte a variáveis de ambiente e documentação da API com Swagger
    - Atualizado o package.json e package-lock.json para incluir a biblioteca dotenv.
    - Configurado o uso do dotenv no main.ts para carregar variáveis de ambiente.
    - Adicionadas anotações Swagger nos controladores de Form e User para documentação das rotas.
    - Atualizados DTOs de criação de Form e User para incluir novos campos e anotações Swagger.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './modules/user/user.module';
+import { FormModule } from './modules/form/form.module';
 
 @Module({
-  imports: [],
+  imports: [UserModule, FormModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/modules/form/form.module.ts
+++ src/modules/form/form.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { FormController } from './controller/form.controller';
 import { FormService } from './service/form.service';
 import { PrismaFormRepository } from './repository/prisma-form.interface';
-import { PrismaService } from 'prisma/prisma.service';
+import { PrismaService } from '../../../prisma/prisma.service';
 
 @Module({
   controllers: [FormController],
